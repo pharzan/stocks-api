1ac0e2633f62c292fc97665e6c60a1dfe0570e34
Farzan Tinati <farzan.tinati@justsnap.de>
2022-02-28 21:46:39 +0300

    added more complex queries for stats and stock by symbol route

--- src/routes/stocks.ts
+++ src/routes/stocks.ts
@@ -1,23 +1,24 @@
-const stocks=async (fastify, opts, next) => {
+const stocks = async (fastify, opts, next) => {
 
-    fastify.get('/:symbol/price',async (req, res) => {
+    fastify.get('/:symbol/price', async (req, res) => {
 
         const { symbol } = req.params;
         const { start, end } = req.query;
         let results = [];
-        try{
-            results = await fastify.knex('trades')
-            .select('symbol')
-            .max('price as highest')
-            .min('price as lowest')
-            .where('symbol', symbol)
-            .where(function(c){
-                if(start) c.where('timestamp', '>=', start);
-                if(end) c.where('timestamp', '<=', end);
-            }).first().groupBy('symbol');
+        try {
 
-        }catch(e){
-            return res.status(400).send({
+            results = await fastify.knex('trades')
+                .select('symbol')
+                .max('price as highest')
+                .min('price as lowest')
+                .where('symbol', symbol)
+                .where(function (q) {
+                    if (start) q.where('timestamp', '>=', start);
+                    if (end) q.where('timestamp', '<=', end);
+                }).first().groupBy('symbol');
+
+        } catch (e) {
+            return res.status(500).send({
                 response: false,
                 message: 'error getting stock price'
             })
@@ -29,17 +30,50 @@ const stocks=async (fastify, opts, next) => {
         })
     })
 
-    fastify.get('/stats',async (req, res) => {
+    fastify.get('/stats', async (req, res) => {
 
         const { start, end } = req.query;
+        let trades, results = {};
+        try {
+            trades = await fastify.knex('trades')
+                .select('timestamp', 'price', 'symbol')
+                .where(function (q) {
+                    if (start) q.where('timestamp', '>=', start);
+                    if (end) q.where('timestamp', '<=', end);
+                });
 
+                trades.forEach(trade => {
+                    if(results[trade.symbol]) results[trade.symbol].price.push(trade.price);
+                    else results[trade.symbol] = { price: [trade.price] };
+                });
+
+                Object.keys(results).forEach(symbol => {
+                    const trade = results[symbol];
+                    trade.highest = Math.max(...trade.price);
+                    trade.lowest = Math.min(...trade.price);
+                    let fluct = 0;
+
+                    for(let i = 0; i < trade.price.length; i++){
+                        fluct += trade.price[i] !=trade.price[i-1]?1:0;
+                    }
+
+                    trade.fluctuation = fluct;
+                })
+
+        } catch (e) {
+            return res.status(500).send({
+                response: false,
+                message: 'error getting stock price'
+            })
+        }
         return res.status(200).send({
             response: true,
-            message: 'everything is healthy'        
+            message: 'trade statistics',
+            results
         })
     })
-    
-    
+
+
 
     next();
 }
